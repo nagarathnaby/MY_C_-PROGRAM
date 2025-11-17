37bead670ee6ce440f1aec54f126e35ea3161199
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-18 00:21:35 +0530

    Create Enter_value.c

---
+++ Enter_value.c
@@ -0,0 +1,17 @@
+// Modify to enter user values
+
+int a = 10, b = 20;
+printf("Sum = %d", a + b);
+
+
+
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter the value of a and b");
+    scanf("%d%d",&a,&b);
+    
+    printf("Sum = %d", a + b);
+    return 0;
+}
