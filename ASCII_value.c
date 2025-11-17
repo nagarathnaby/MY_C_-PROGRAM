5833164067ae2e84a58cf407cd39fa97bf86319c
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-18 00:15:08 +0530

    Create ASCII_value.c

---
+++ ASCII_value.c
@@ -0,0 +1,12 @@
+// ASCII value
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+    
+    printf("ASCII value of %c is %d",ch,ch);
+    return 0;
+}
