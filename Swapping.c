4b51587b262acae169f719150014fe1f53f23c27
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-18 00:30:52 +0530

    Create Swapping.c

---
+++ Swapping.c
@@ -0,0 +1,23 @@
+// Swapping without temp variable
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter value of a: ");
+    scanf("%d", &a);
+
+    printf("Enter value of b: ");
+    scanf("%d", &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("\nAfter swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
