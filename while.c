32f5bd72373915a80c985149c423d611b94bd1e7
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-18 11:56:48 +0530

    Create while.c

---
+++ while.c
@@ -0,0 +1,16 @@
+// for loop
+
+#include <stdio.h>
+
+int main() {
+    int a=5;
+    
+    while(a<5)
+    {
+        printf("a is even");
+        a++;
+    }
+    printf("a is odd");
+    
+    return 0;
+}
