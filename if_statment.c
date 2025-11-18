b35fa5924910f2084aded30a62ccc9ccd98dc3fb
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-18 10:55:12 +0530

    Create if_statment.c

---
+++ if_statment.c
@@ -0,0 +1,15 @@
+//if statment
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter the value of a\n");
+    scanf("%d",&a);
+    if("a>=0")
+    {
+        printf("a is positive");
+    }
+  
+    return 0;
+}
