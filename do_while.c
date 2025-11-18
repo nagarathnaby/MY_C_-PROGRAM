0f65c61cb4a34a6a18243b830f4f5b595d8a7a10
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-18 12:13:07 +0530

    Create do_while.c

---
+++ do_while.c
@@ -0,0 +1,16 @@
+// do while
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("enter the value of a");
+    scanf("%d",&a);
+   do
+    {
+        printf("a is positive\n");
+        a++;
+    }
+   while(a>0);
+    
+    return 0;
+}
