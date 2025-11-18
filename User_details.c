90e48eb0871cced48a2e302120ef8cc0defce541
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-18 15:44:51 +0530

    Create User_details.c

---
+++ User_details.c
@@ -0,0 +1,19 @@
+// User details
+
+#include<stdio.h>
+int User_details()
+{
+    char name,dep;
+    int age;
+    
+    printf("enter your name\n");
+    scanf("%s",&name);
+    printf("enter your age\n");
+    scanf("%d",&age);
+    printf("enter your department\n");
+    scanf("%s",&dep);
+    
+}
+int main(){
+    User_details();
+}
