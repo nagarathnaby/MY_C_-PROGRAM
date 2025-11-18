727c24fb4635c81481ee740a1fb132a66aa93d40
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-18 11:23:56 +0530

    Create if_else_ladder.c

---
+++ if_else_ladder.c
@@ -0,0 +1,20 @@
+// if else ladder
+
+#include <stdio.h>
+
+int main() {
+    int marks;
+    scanf("%d",&marks);
+    printf("Enter your marks");
+    
+    if(marks>=90)
+    printf("Grade A");
+    else if(marks>=80)
+    printf("Grade B");
+    else if(marks>=70)
+    printf("Grade C");
+    else 
+    printf("Grade D");
+
+    return 0;
+}
