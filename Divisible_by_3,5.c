094de21d1e8c11203bbcb6e2014442f1f1bf4173
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-19 16:23:19 +0530

    Update Divisible_by_3,5.c

--- Divisible_by_3,5.c
+++ Divisible_by_3,5.c
@@ -7,10 +7,10 @@ int main() {
     printf("Enter the value of a");
     scanf("%d",&a);
     if(a%3==0){
-        printf("a is divisible by 3");
+        printf("a is divisible by 3\n");
     }
     if(a%5==0){
-        printf("a is divisible by 5");
+        printf("a is divisible by 5\n");
     }
     
     return(0);
