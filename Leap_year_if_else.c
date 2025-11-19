fe044da1f83f238d9aa2dc39586c13e482a2e8cd
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-19 21:56:27 +0530

    Create Leap_year_if_else.c

---
+++ Leap_year_if_else.c
@@ -0,0 +1,16 @@
+//Leap year
+
+#include<stdio.h>
+int main(){
+    int year;
+    printf("Enter the year : ");
+    scanf("%d",&year);
+    
+    if((year % 400 == 0) || (year % 4 == 0 ) && (year % 100 != 0)){
+        printf("%d is a leap year.\n");
+    }
+    else
+    {
+        printf("%d is not a leap year.\n");
+    }
+}
