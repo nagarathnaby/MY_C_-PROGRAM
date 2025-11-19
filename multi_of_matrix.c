5fa9eee8db610f9b8fbfa4f4919c96e6bcb0b696
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-19 12:18:17 +0530

    Create multi_of_matrix.c

---
+++ multi_of_matrix.c
@@ -0,0 +1,38 @@
+// multi of matrix
+
+#include<stdio.h>
+int main()
+{
+int row,col;
+printf("Enter the number of row and column");
+scanf("%d%d",&row,&col);
+
+int a[row][col], b[row][col], c[row][col];
+    printf("Enter the elements of matrix A:");
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+    printf("Enter the elements of matrix B:");
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            c[i][j] = a[i][j] * b[i][j];
+        }
+    }
+    
+    printf("Resultant value is\n");
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            printf("%d\n",c[i][j]);
+        }
+    }
+}   
+    
