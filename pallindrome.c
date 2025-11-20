c33d79013bb0c8255ed95e4545aee1775ae3bad7
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-20 11:13:24 +0530

    Create pallindrome.c

---
+++ pallindrome.c
@@ -0,0 +1,21 @@
+//  pallindrome
+
+#include <stdio.h>
+
+int main() {
+    int num, rev=0, ori, rem;
+    printf("Enter a number\n");
+    scanf("%d",&num);
+    ori = num;
+    while(num != 0){
+        rem = num % 10;
+        rev = rev*10 + rem;
+        num = num/10;
+    }
+    if(ori == rev){
+        printf("pallindrome");
+    }
+    else{
+        printf("not a pallindrome");
+    }
+}
