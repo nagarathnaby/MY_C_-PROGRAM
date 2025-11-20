9eac306a94e94ff66f70f504cb350858d893789c
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-20 12:05:07 +0530

    Create Palindrom_string.c

---
+++ Palindrom_string.c
@@ -0,0 +1,22 @@
+//  palindrome using string
+
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char str[50], rev[50];
+    int i, len;
+    
+    printf("Enter string\n");
+    scanf("%s",&str);
+    len = strlen(str);
+    for(i = 0; i < len; i++){
+        rev[i] = str[len - 1 - i];
+    }
+    rev[len] = '\0';
+    if(strcmp(str,rev)==0){
+        printf("palindrome");
+    }
+    else{
+        printf("Not a palindrome");
+    }
+}
