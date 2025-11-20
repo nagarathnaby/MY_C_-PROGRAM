d30bea225fcfbae1d1166e1bd2df92fd74ca4702
nagarathnaby <nagarathnaby27@gmail.com>
2025-11-20 12:59:21 +0530

    Create Call_by_reference.c

---
+++ Call_by_reference.c
@@ -0,0 +1,19 @@
+// Call by reference
+
+#include <stdio.h>
+
+int main() {
+    int a=10, b=20;
+    int *p1, *p2, temp;
+    
+    p1=&a;
+    p2=&b;
+    
+    printf("Before : %d %d\n",a ,b);
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+    printf("After : %d %d\n", a, b);
+    
+    return 0;
+}
